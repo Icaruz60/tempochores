4625c999c00ba3a987f8cc2f4e8748f9721ca0c1
Chris Smith <chris.smith-6@selu.edu>
2025-10-18 19:33:08 -0500

    updated planTempoChore page to use seconds

--- tempochores_app/lib/pages/plan_tempochore_page.dart
+++ tempochores_app/lib/pages/plan_tempochore_page.dart
@@ -77,7 +77,7 @@ class _PlanTempoChorePageState extends State<PlanTempoChorePage> {
 
     for (final c in [...high, ...med, ...low]) {
       final dur = c.averageDuration;
-      if (dur.inMinutes > 0 && dur <= remaining) {
+      if (dur.inSeconds > 0 && dur <= remaining) {
         chosen.add(c);
         remaining -= dur;
       }
@@ -233,7 +233,7 @@ class _PlanTempoChorePageState extends State<PlanTempoChorePage> {
             // TIMER DISPLAY
             Text(
               timer.running
-                  ? '${timer.remaining.inMinutes}:${(timer.remaining.inSeconds % 60).toString().padLeft(2, '0')} left'
+                  ? '${timer.remaining.inSeconds}:${(timer.remaining.inSeconds % 60).toString().padLeft(2, '0')} left'
                   : (timer.remaining > Duration.zero
                       ? ''
                       : 'Timer stopped'),
